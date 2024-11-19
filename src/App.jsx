aa14d7d08903485d2b2ff0fa54311eeda9cddb8b
thirupathi8 <124600966+thirupathi8@users.noreply.github.com>
2024-11-19 21:20:00 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Login from './Components/Login'
 import Input from './input'
 import Analytics from './Analytics'
 import './App.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
   const [isSignedUp, setIsSignedUp] = useState(false)
@@ -17,6 +17,36 @@ function App() {
   const handleLogin = () => {
     setIsLoggedIn(true)
   }
+
+  const checkSession = async () => {
+    const token = localStorage.getItem("sessionToken")
+    if (token) {
+      try {
+        const response = await fetch("http://localhost:3000/user/validate-token", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+          },
+        }
+        )
+
+        if (response.ok) {
+          setIsLoggedIn(true)
+        }
+        else {
+          localStorage.removeItem("sessionToken")
+        }
+      }
+      catch (error) {
+        console.error("Error validating token: ", error)
+      }
+    }
+  }
+
+  useEffect(() => {
+    checkSession()
+  }, [])
 
   return (
     <div className="App">
